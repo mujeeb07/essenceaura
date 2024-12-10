5e1aef7038243e701155ac620e46495a67e7972d
mujib18122023 <mujeebalp742@gmail.com>
2024-12-10 20:30:41 +0530

    connected with atlas

--- config/database_config.js
+++ config/database_config.js
@@ -1,11 +1,29 @@
+// const mongoose = require('mongoose');
+// console.log("mongos db check")
+// const connectDB = async () => {
+//     const uri = "mongodb+srv://cluster_database:xkgOKOhmbhpvB2Y8@cluster0.bg922.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+//     try {
+//         await mongoose.connect(uri);
+//         console.log("Database connected successfully");
+//     } catch (error) {
+//         console.error("Error connecting to MongoDB", error);
+//     }
+// };
+// module.exports = connectDB;
+
+
 const mongoose = require('mongoose');
+console.log("Checking MongoDB connection...");
+
 const connectDB = async () => {
-    const uri = process.env.DATABASE_CONFIG 
+    const uri = "mongodb+srv://mujibrehman8846:NQXWUYgpPzrUBTtj@essencecluster.ta8gt.mongodb.net/?retryWrites=true&w=majority&appName=essenceCluster";
     try {
         await mongoose.connect(uri);
         console.log("Database connected successfully");
     } catch (error) {
-        console.error("Error connecting to MongoDB", error);
+        console.error("Error connecting to MongoDB:", error.message);
     }
 };
+
 module.exports = connectDB;
+
