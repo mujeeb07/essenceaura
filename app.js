c46200899babfbd9ba3ac248cb18dbef45d01d19
mujib18122023 <mujeebalp742@gmail.com>
2025-01-07 14:32:40 +0530

    changes in app

--- app.js
+++ app.js
@@ -3,19 +3,22 @@ const session_config = require("./config/session_config");
 const passport = require("./config/passport");
 require("dotenv").config();
 const connect_db = require("./config/database_config");
-const user_route = require("./routes/user/user_route");   
+const user_route = require("./routes/user/user_route");
 const admin_route = require("./routes/admin/admin_route");
 const file_upload = require("express-fileupload");
 const path = require("path");
-const { handleNotFound, handleServerError } = require('./middleware/error_handler');
+const {
+  handleNotFound,
+  handleServerError,
+} = require("./middleware/error_handler");
 
 const app = express();
 
-connect_db(); 
+connect_db();
 
 app.use(session_config);
 
-const PORT = 4000 || process.env.PORT;
+const PORT = process.env.PORT;
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.static("public"));
@@ -26,11 +29,8 @@ app.use(
     limits: { fileSize: 50 * 1024 * 1024, files: 6 },
     useTempFiles: true,
     tempFileDir: path.join(__dirname, "public/tmp"),
-  })   
+  })
 );
-
-
-
 
 app.use("/", user_route);
 app.use("/admin", admin_route);
